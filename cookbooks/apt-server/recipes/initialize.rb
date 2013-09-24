6d6bb3483af702d344e3bcfa95ec7d1cd7d11f39
LNR <simhaece@gmail.com>
2013-09-24 19:02:55 +0530

    apt-server cookbook

---
+++ cookbooks/apt-server/recipes/initialize.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: apt-server
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'nginx'
+service 'nginx' do
+	supports [:status]
+	action :start
+end
+package 'dpkg-dev'
+directory "/usr/share/nginx/www/deb_repo/binary" do
+  owner "ubuntu"
+  group "ubuntu"
+  mode 0755
+  action :create
+end
